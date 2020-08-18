734f409df3b0e75f2061c5f77bd7d3e6db261a2a
DenisKalyanov <denzarj@mail.ru>
2020-08-18 13:41:42 +0300

    create folders with middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,18 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-// определим функцию которая будет этим самым middleware
-// req - объект запроса
-// reы - объект ответа
-// next
-module.exports = (req, res, next) =>{
-  // из заголовка запроса получаем токен "auth-token" – внутри его токен
-  // так у объекта запроса, мы с помощью метода header получим содержимое
+module.exports = (req, res, next) => {
   const token = req.header("auth-token");
 
-  //далее мы смотрим, если токена вообще нету (юзер не зареган),
-  if(!token){
-    return res.status(401).json({msg:"No token, authrolizatoin denied"});
-    // если его нет, прокинем 401 ошибку
-    // сообщаем в формате json, объектом, со свойством msg: "токена нет, не получишь данные"
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authrolizatoin denied" });
   }
-  // оборачиваем в try/catch, если вернётся ошибка, то обработаем её, 
-  //если всё гуд, то попадёт в try
-  try{
-    // если всё нормльно, то мы раздекодируем токен.
-    // создадим переменную, где будем хранить инфу о раздекодированом токене
-    // у jwt есть метод спеиальный verify (токен, секрет по которому декодировали)
+  try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
 
-    // если всё гуд, то мы в объект req положим свойство user, со значением decoded.user
     req.user = decoded.user;
-  }catch(err){
-    // если вохникла ошибка
-    // return пишем когда нужно полностью выйти из кода уже
-    return res.status(401).json({msg: "Token is not valid"})
+    next();
+  } catch (err) {
+    return res.status(401).json({ msg: "Token is not valid" });
   }
-
-}
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+// Импортируем пользователя чтобы его искать в базе данных
+const User = require("../../models/User")
 // @route GET api/auth
-// @desc Test route
-// access Public
+// @desc Get a user
+// access Private
 
 router.get("/", (req, res) => {
-  res.send("Auth route");
+  try {
+    // сохраним пользователя в объекте юзер, обратимся к схеме и его статическому методу поиска по ИД
+    // а метод .select("поле которое исключим")
+    const user = await User.findById(req.user.id).select("-password");
+
+  } catch (err) {
+    //пишем логирование ошибки
+    console.error(err.message);
+    // и отправим статус 500 и сообщение
+    // не пишем return потому что мы отправили ошибку и больше ничего не будет происходить,
+    // полностью выйдем из опперации.
+    res.status(500).send("Server error")
+  }
 });
 
 module.exports = router;
