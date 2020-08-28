54cba7f5858b209e3f168c971432f4150f36bb9b
DenisKalyanov <denzarj@mail.ru>
2020-08-29 01:19:20 +0300

    was create routes for add information about education and eexperiance of user, and routes for delete them

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -179,4 +179,148 @@ router.delete("/", authMiddleware, async (req, res) => {
   }
 });
 
+// @route PATCH api/profile/experience
+// @desc Add profile experience
+// access Private
+router.patch(
+  "/experience",
+  [
+    authMiddleware,
+    check("title", "Title is required").not().isEmpty(),
+    check("company", "Company is required").not().isEmpty(),
+    check("from", "From date is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // если всё норм, ошибок нет, то нам нужно задеструкторизировать наш req.body
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+    // на строке выше, мы получили перемеенные, для того чтобы в будущем могли их как-то обработать.
+    //И внизу используем
+    const newExp = { title, company, location, from, to, current, description };
+
+    try {
+      //находим профиль по id который нам нужен
+      const profile = await Profile.findOne({ user: req.user.id });
+      // обращаемся к объекту который мы нашли и к его свойству experience
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc DELETE experience from profile
+// access Private
+router.delete("/experience/:exp_id", authMiddleware, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // теперь нам нужно найти тот experience который нужно удалить
+    // И в нём будем перебирать массив experienc, состоящий из объектов.
+    // Их назовём exp и будем его сравнивать с тем, что мы передаём в параметрах запроса
+    // в адресной строке.
+
+    const removeIndex = profile.experience.findIndex(
+      (exp) => exp._id.toString() === req.params.exp_id
+    );
+
+    if (removeIndex === -1) {
+      return res.status(404).json({ msg: "Experiance not found" });
+    }
+    // далее вызываем метот splice, Где указываем с какой позиции и сколько элементов удалить
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route PATCH api/profile/education
+// @desc Add profile education
+// access Private
+router.patch(
+  "/education",
+  [
+    authMiddleware,
+    check("school", "School is required").not().isEmpty(),
+    check("degree", "Degree is required").not().isEmpty(),
+    check("fieldofstudy", "Field of stady is require").not().isEmpty(),
+    check("from", "From date is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // если всё норм, ошибок нет, то нам нужно задеструкторизировать наш req.body
+    const newEdu = ({
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body);
+
+    try {
+      //находим профиль по id который нам нужен
+      const profile = await Profile.findOne({ user: req.user.id });
+      // обращаемся к объекту который мы нашли и к его свойству experience
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route DELETE api/profile/education/:exp_id
+// @desc DELETE education from profile
+// access Private
+router.delete("/education/:edu_id", authMiddleware, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // теперь нам нужно найти тот education который нужно удалить
+    // И в нём будем перебирать массив experienc, состоящий из объектов.
+    // Их назовём exp и будем его сравнивать с тем, что мы передаём в параметрах запроса
+    // в адресной строке.
+
+    const removeIndex = profile.education.findIndex(
+      (edu) => edu._id.toString() === req.params.edu_id
+    );
+
+    if (removeIndex === -1) {
+      return res.status(404).json({ msg: "Education not found" });
+    }
+    // далее вызываем метот splice, Где указываем с какой позиции и сколько элементов удалить
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
