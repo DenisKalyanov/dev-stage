212d70cfd1af5a54321b449798a9b82021a43564
DenisKalyanov <denzarj@mail.ru>
2020-10-01 01:06:20 +0300

    was created components for Login, navigation, register. And added styles for basic route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Router >
+  );
 }
 
-export default App
+export default App 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
+ReactDom.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
